9bf763f07d610ef1a6587cc0ccbd5f9ab9ce9e01
Jan Kundrát <jan.kundrat@telecominfraproject.com>
2019-10-23 14:16:24 +0200

    looks like I can generate stuff now

--- generate-tip-demo.py
+++ generate-tip-demo.py
@@ -17,7 +17,10 @@ def mk_edfa(name, gain, voa=0.0):
 
 def add_att(a, b, att):
     global J
-    uid = f"att-{a}-{b}"
+    if att > 0:
+        uid = f"att-({a})-({b})"
+    else:
+        uid = f"splice-({a})-({b})"
     J["elements"].append(
         {"uid": uid, "type": "Fused", "params": {"loss": att}},
     )
@@ -49,7 +52,7 @@ def build_fiber(city1, city2):
 
 def unidir_patch(a, b):
     global J
-    uid = f"patch_{a}_{b}"
+    uid = f"patch-({a})-({b})"
     J["elements"].append(
         {
             "uid": uid,
@@ -69,79 +72,47 @@ def unidir_patch(a, b):
     #unidir_join(a, uid)
     #unidir_join(uid, b)
 
-#for CITY in (AMS, BRE, COL):
-for CITY in (AMS, BRE,):
+#for CITY in (AMS, BRE,):
+for CITY in (AMS, BRE, COL):
     # transceivers
     J["elements"].append(
         {"uid": f"trx-{CITY}", "type": "Transceiver"}
     )
-
     J["elements"].append(
         {"uid": f"roadm-{CITY}-AD", "type": "Roadm", "params": {"target_pch_out_db": -12.5}}
     )
+    unidir_join(f"trx-{CITY}", f"roadm-{CITY}-AD")
+    unidir_join(f"roadm-{CITY}-AD", f"trx-{CITY}")
 
-    #mk_edfa(f"roadm-{CITY}-AD-add", 22, 22)
-    #mk_edfa(f"roadm-{CITY}-AD-drop", 27, 10)
-    #unidir_patch(f"trx-{CITY}", f"roadm-{CITY}-AD-add")
-    #unidir_patch(f"roadm-{CITY}-AD-drop", f"trx-{CITY}")
-    #unidir_patch(f"roadm-{CITY}-AD-add", f"roadm-{CITY}-AD")
-    #unidir_patch(f"roadm-{CITY}-AD", f"roadm-{CITY}-AD-drop")
-
-    unidir_patch(f"trx-{CITY}", f"roadm-{CITY}-AD")
-    unidir_patch(f"roadm-{CITY}-AD", f"trx-{CITY}")
-
-    #J["elements"].append(
-    #    {"uid": f"att-trx-{CITY}", "type": "Fused", "params": {"loss": 22}},
-    #)
-    #unidir_join(f"trx-{CITY}", f"att-trx-{CITY}")
-    #unidir_join(f"att-trx-{CITY}", f"roadm-{CITY}-AD-add")
-    #unidir_join(f"roadm-{CITY}-AD-drop", f"att-trx-{CITY}")
-    #unidir_join(f"att-trx-{CITY}", f"trx-{CITY}")
     for n in (1,2):
         J["elements"].append(
             {"uid": f"roadm-{CITY}-L{n}", "type": "Roadm", "params": {"target_pch_out_db": -12.5}}
         )
         mk_edfa(f"roadm-{CITY}-L{n}-booster", 22)
         mk_edfa(f"roadm-{CITY}-L{n}-preamp", 27)
+        unidir_join(f"roadm-{CITY}-L{n}", f"roadm-{CITY}-L{n}-booster")
+        unidir_join(f"roadm-{CITY}-L{n}-preamp", f"roadm-{CITY}-L{n}")
+
         unidir_patch(f"roadm-{CITY}-AD", f"roadm-{CITY}-L{n}")
         unidir_patch(f"roadm-{CITY}-L{n}", f"roadm-{CITY}-AD")
-        unidir_patch(f"roadm-{CITY}-L{n}", f"roadm-{CITY}-L{n}-booster")
-        unidir_patch(f"roadm-{CITY}-L{n}-preamp", f"roadm-{CITY}-L{n}")
         for m in (1,2):
             if m == n:
                 continue
-            #add_att(f"roadm-{CITY}-L{n}-preamp", f"roadm-{CITY}-L{m}-booster", 22)
-            #unidir_join(f"roadm-{CITY}-L{n}-preamp", f"roadm-{CITY}")
-            #unidir_join(f"roadm-{CITY}", f"roadm-{CITY}-L{m}-booster")
+            #add_att(f"roadm-{CITY}-L{n}", f"roadm-{CITY}-L{m}", 22)
+            unidir_patch(f"roadm-{CITY}-L{n}", f"roadm-{CITY}-L{m}")
 
 #for LONG in ((AMS, BRE), (BRE, AMS)):
 #for LONG in ((AMS, BRE),):
 #for LONG in ((AMS, BRE), (BRE, COL)):
-#for LONG in ((AMS, BRE), (BRE, COL), (COL, AMS)):
-for LONG in ((AMS, BRE), (BRE, AMS),):
-    city1, city2 = LONG
+#for city1, city2 in ((AMS, BRE), (BRE, AMS),):
+for city1, city2 in ((AMS, BRE), (BRE, COL), (COL, AMS)):
     build_fiber(city1, city2)
     unidir_join(f"roadm-{city1}-L1-booster", f"fiber-{city1}-{city2}")
-    unidir_join(f"fiber-{city1}-{city2}", f"roadm-{city1}-L2-preamp")
+    unidir_join(f"fiber-{city1}-{city2}", f"roadm-{city2}-L2-preamp")
     build_fiber(city2, city1)
     unidir_join(f"roadm-{city2}-L2-booster", f"fiber-{city2}-{city1}")
     unidir_join(f"fiber-{city2}-{city1}", f"roadm-{city1}-L1-preamp")
 
-
-#unidir_join("roadm-Amsterdam-L1-booster", "fiber-Amsterdam-Bremen")
-#unidir_join("fiber-Amsterdam-Bremen", "roadm-Bremen-L1-preamp")
-#unidir_join("roadm-Bremen-L1-booster", "fiber-Amsterdam-Bremen")
-#unidir_join("fiber-Amsterdam-Bremen", "roadm-Amsterdam-L1-preamp")
-#
-#unidir_join("roadm-Bremen-L2-booster", "fiber-Bremen-Cologne")
-#unidir_join("fiber-Bremen-Cologne", "roadm-Cologne-L2-preamp")
-#unidir_join("roadm-Cologne-L2-booster", "fiber-Bremen-Cologne")
-#unidir_join("fiber-Bremen-Cologne", "roadm-Bremen-L2-preamp")
-#
-#unidir_join("roadm-Cologne-L1-booster", "fiber-Cologne-Amsterdam")
-#unidir_join("fiber-Cologne-Amsterdam", "roadm-Amsterdam-L2-preamp")
-#unidir_join("roadm-Amsterdam-L2-booster", "fiber-Cologne-Amsterdam")
-#unidir_join("fiber-Cologne-Amsterdam", "roadm-Cologne-L1-preamp")
 
 for _, E in enumerate(J["elements"]):
     uid = E["uid"]
